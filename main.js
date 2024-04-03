c07edd256fd7797f6521158be8eb59d55b152224
Marcus Leong <marcusleongjx@gmail.com>
2024-04-03 14:04:37 +0800

    Add event listener for all buttons

--- main.js
+++ main.js
@@ -29,3 +29,23 @@ function operate(first, second, op) {
         divide(first, second);
     }
 }
+
+function addToDisplay() {
+    const added = document.createAttribute("p");
+    display.appendChild(added);
+}
+
+const display = document.querySelector("#display")
+const btnList = document.querySelectorAll("button")
+console.log(btnList);
+for (let i=0; i<btnList.length-1; i++) {
+    let //how to change this to a changing variable name? btnn = document.querySelector(btnList[i]);
+    //changing variable name.addEventListener('click', () => addToDisplay())
+}
+const testBtn = document.querySelector("button#zero");
+testBtn.addEventListener('click', () => {
+    console.log("0");
+})
+//btn.addEventListener('click', () => {
+    //display.textContent = "0";
+//})
