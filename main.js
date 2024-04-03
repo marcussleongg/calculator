d7f9bfeaeda9776ae40fd840b578eaf9e021bf28
Marcus Leong <marcusleongjx@gmail.com>
2024-04-03 11:22:07 +0800

    Add html boilerplate, buttons, and display

--- main.js
+++ main.js
@@ -17,3 +17,15 @@ function divide(a, b) {
 let firstNum;
 let secondNum;
 let operator;
+
+function operate(first, second, op) {
+    if (op == '+') {
+        add(first, second);
+    } else if (op == '-') {
+        subtract(first, second);
+    } else if (op == '*') {
+        multiply(first, second);
+    } else {
+        divide(first, second);
+    }
+}
